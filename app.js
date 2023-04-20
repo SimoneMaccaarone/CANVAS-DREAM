1f1bfdb7de1076fd615d201fd0a3a34b07db75ca
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-20 17:33:39 +0200

    copy canvas-1

--- app.js
+++ app.js
@@ -1,2 +1,44 @@
 const myCanvas = document.getElementById('my-canvas');
-const ctx = myCanvas.getContext('2d')
+const ctx = myCanvas.getContext('2d');
+
+
+//--- Creation Point/pixel
+const point = {
+    x: 300,
+    y: 300
+}
+
+
+setInterval(() => {
+
+    const randomX = (Math.random() * 4) - 2;
+    const randomY = (Math.random() * 4) - 2;
+
+
+    ctx.fillStyle = getRandomColorRGB();
+    ctx.fillRect(point.x, point.y, 2, 2);
+
+    point.x += randomX;
+    point.y += randomY;
+
+}, 1);
+
+
+
+function getRandomColorRGB() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const alpha = Math.random();
+
+    return ctx.fillStyle = `rgba(${red},${green},${blue}`
+}
+
+function getRandomColorRGBA() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const alpha = Math.random();
+
+    return ctx.fillStyle = `rgba(${red},${green},${blue},${alpha})`
+}
