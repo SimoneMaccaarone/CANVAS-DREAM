d5894318cfe688a3734e2c4148ed9f4110e03845
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-22 19:16:33 +0200

    copy canvas-1

--- app.js
+++ app.js
@@ -1,81 +1,29 @@
 const myCanvas = document.getElementById('my-canvas');
 const ctx = myCanvas.getContext('2d');
 
+let newGeneration = Generation.generateRandom(100, myCanvas.width, myCanvas.height);
+console.log(newGeneration);
 
-// //--- Creation Point/pixel
-// const point = {
-//     x: Math.random()*600,
-//     y: Math.random()*600
-// }
 
-for (let i = 0; i < 100; i++) {
+function step() {
 
-    const point = {
-        x: Math.random() * 600,
-        y: Math.random() * 600
+    newGeneration.draw(ctx);
+
+    if (newGeneration.isDead()) {
+        ctx.fillStyle = 'rgba(0,0,0,0.5)'
+        ctx.fillRect(0, 0, myCanvas.width, myCanvas.height)
+        newGeneration = Generation.generateRandom(100, myCanvas.width, myCanvas.height);
     }
-    
-   point.color =  colorRandomRGB(); // colore del pennarello
-
-// cordinate random che si somma a "const point"
-//  ----- Cordinate random ----
-    setInterval(() => {
-        const randomX = (Math.random()*4)-2;
-        const randomY = (Math.random()*4)-2;
-
-        ctx.fillStyle= point.color
-        ctx.fillRect(point.x, point.y,2,2)
-
-        point.x+= randomX
-        point.y+= randomY
-    }, 1);
+    window.requestAnimationFrame(step);
 }
 
-setInterval(() => {
-    const newCanvas = document.getElementById('new-canvas');
-    const newCtx = newCanvas.getContext('2d');
+window.requestAnimationFrame(step);
 
-    newCtx.color = trasparentForWarm()
-}, 1);
-
-// setInterval(() => {
-
-//     const randomX = (Math.random() * 4) - 2;
-//     const randomY = (Math.random() * 4) - 2;
-
-
-//     ctx.fillStyle = getRandomColorRGB();
-//     ctx.fillRect(point.x, point.y, 2, 2);
-
-//     point.x += randomX;
-//     point.y += randomY;
-
-// }, 1);
-
-
-
-function colorRandomRGB() {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-
-    return ctx.fillStyle = `rgb(${red},${green},${blue})`
-}
-
-function colorRandomRGBA() {
+function getRandomColorRGBA() {
     const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
     const alpha = Math.random();
-
-    return ctx.fillStyle = `rgba(${red},${green},${blue},${alpha})`
-}
-
-function trasparentForWarm() {
-    const red = 0;
-    const green = 0;
-    const blue = 0;
-    const alpha = 0.3;
-
-    return ctx.fillStyle = `rgba(${red},${green},${blue},${alpha})`
-}
+    
+    return ctx.fillStyle = `rgba(${red},${green},${blue},${alpha})`;
+}  
---
+++ point.js
@@ -0,0 +1,72 @@
+class Point {
+    //     Creation Point/pixel
+    constructor(x, y, speedX, speedY, color) {
+        this.x = x;
+        this.y = y;
+        this.speedX = speedX;
+        this.speedY = speedY;
+        this.color = color;
+        this.forceX = (Math.random() * 2) - 1;
+        this.forceY = (Math.random() * 2) - 1;
+
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = (this.color);
+        ctx.fillRect(this.x, this.y, 1, 1);
+    }
+
+    static generateRandom(canvasWidth, canvasHeight) {
+        //               X & Y
+        const randomX = 300;
+        const randomY = 300;
+
+        // //           Speed X & Y
+        const speedX = (Math.random() * 2) - 1
+        const speedY = (Math.random() * 2) - 1
+
+        //               Color
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        const randomColor = `rgba(${red},${green},${255}`;
+
+        const newPoint = new Point(randomX, randomY, speedX, speedY, randomColor);
+
+        return newPoint;
+    }
+
+    changePosition() {
+
+        if (Math.random() > 0.9) {
+            this.forceX = (Math.random() * 0.2) - 0.1;
+        }
+
+        if (Math.random() > 0.9) {
+            this.forceY = (Math.random() * 0.2) - 0.1;
+        }
+
+
+        this.speedX += this.forceX;
+        this.speedY += this.forceY;
+
+        if (this.speedX > 1) {
+            this.speedX = 1;
+        }
+        if (this.speedX < -1) {
+            this.speedX = -1;
+        }
+
+        if (this.speedY > 1) {
+            this.speedY = 1;
+        }
+        if (this.speedY < -1) {
+            this.speedY = -1;
+        }
+
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+    }
+
+}
